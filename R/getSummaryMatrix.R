3e6fcd349dd348de36109a25a15b9d2d0c5aeda2
junjypark <73309648+junjypark@users.noreply.github.com>
2022-03-02 16:05:11 -0500

    Update getSummaryMatrix.R

--- R/getSummaryMatrix.R
+++ R/getSummaryMatrix.R
@@ -1,3 +1,23 @@
+spLeverage=function(ymat, distMat=NULL, mod0=NULL, J=50){
+  if (!is.null(mod0)){
+    data=t(lm(t(data)~mod0)$residuals)
+  } else{
+    cat("mod0 has not been specified. Assuming the test for the mean.\n")
+  }
+  
+  if (!is.null(distMat)){
+    covreg.fit=CovReg(data, distMat)
+    NNGPinfo=constructNNGPinfo(distMat, NN=50)
+    NNGPprec=buildNNGPmat(distMat, NNGPinfo, covreg.fit)$NNGPprec
+    data.new=NNGPprec%*%data
+  } else{
+    cat("distMat has not been specified. Obtaining residuals wihtout leveraging...\n")
+  }
+  
+  return(list(out=data.new, params.covariance=covreg.fit,
+              NNGPprec=NNGPprec, NNGPinfo=NNGPinfo))
+}
+
 getSummaryMatrix=function(ymat, X=NULL, mask,
                           parallel=F, ncores=1){
   mask.index=which(mask!=0)
