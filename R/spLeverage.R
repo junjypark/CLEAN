bee4c8e8847d8615d78f0fdcae72d6f7fc8fa970
junjypark <73309648+junjypark@users.noreply.github.com>
2022-04-08 16:29:00 -0400

    Update spLeverage.R

--- R/spLeverage.R
+++ R/spLeverage.R
@@ -7,7 +7,7 @@ spLeverage=function(data, distMat=NULL, mod0=NULL, J=50){
   
   if (!is.null(distMat)){
     covreg.fit=CovReg(data, distMat)
-    NNGPinfo=constructNNGPinfo(distMat, NN=50)
+    NNGPinfo=constructNNGPinfo(distMat, NN=J)
     NNGPprec=buildNNGPmat(distMat, NNGPinfo, covreg.fit)$NNGPprec
     data.new=as.matrix(NNGPprec%*%data)
   } else{
@@ -17,31 +17,3 @@ spLeverage=function(data, distMat=NULL, mod0=NULL, J=50){
   return(list(out=data.new, params.covariance=covreg.fit,
               NNGPprec=NNGPprec, NNGPinfo=NNGPinfo))
 }
-
-# getSummaryMatrix=function(ymat, X=NULL, mask,
-#                           parallel=F, ncores=1){
-#   mask.index=which(mask!=0)
-#   ymat=ymat[mask.index,]
-#   p=nrow(ymat); n=ncol(ymat)
-#   
-#   if (!is.null(X)){
-#     n=nrow(X);
-#     Q=diag(n)-tcrossprod(tcrossprod(X,solve(crossprod(X))),X)
-#     ymat=tcrossprod(ymat,Q)
-#   }
-#   
-#   if (isTRUE(parallel)){
-#     cl=makeCluster(ncores)
-#     registerDoParallel(cl)
-#     out=foreach(i=1:p, .combine="rbind")%dopar%{
-#       ymat[i,]/sum(ymat[i,]^2,na.rm=T)
-#     }
-#     stopCluster(cl)
-#   } else{
-#     out=ymat
-#     for (i in 1:p){
-#       out[i,]=out[i,]/sum(out[i,]^2,na.rm=T)
-#     }
-#   }
-#   return(out)
-# }
