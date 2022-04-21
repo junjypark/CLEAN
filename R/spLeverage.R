be71985dcc952787dc3a561a6fd8d2e47fe02546
junjypark <73309648+junjypark@users.noreply.github.com>
2022-04-21 14:10:09 -0400

    Update spLeverage.R

--- R/spLeverage.R
+++ R/spLeverage.R
@@ -1,12 +1,14 @@
 spLeverage=function(data, distMat=NULL, mod0=NULL, J=50){
   if (!is.null(mod0)){
     data=t(lm(t(data)~mod0)$residuals)
+    q=ncol(mod0)
   } else{
+    q=0
     cat("[CLEAN] mod0 has not been specified. Assuming the test for the mean.\n")
   }
   
   if (!is.null(distMat)){
-    covreg.fit=CovReg(data, distMat)
+    covreg.fit=CovReg(data, distMat, n.covariates=q)
     NNGPinfo=constructNNGPinfo(distMat, NN=J)
     NNGPprec=buildNNGPmat(distMat, NNGPinfo, covreg.fit)$NNGPprec
     data.new=as.matrix(NNGPprec%*%data)
