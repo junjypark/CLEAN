a1ccee5fb0efba6c4111d290b10de86bd2916afd
junjypark <73309648+junjypark@users.noreply.github.com>
2023-05-20 00:01:45 -0400

    Update CLEANR.R

--- R/CLEANR.R
+++ R/CLEANR.R
@@ -1,20 +1,52 @@
-CleanR=function(xmat, ymat, NNmatrix, nperm=5000, alpha=0.05,
-                alternative=c("two.sided", "less", "greater"),
-                seed=NULL, npartition=NULL, parallel=F, ncores=1){
-  if (length(which(is(NNmatrix)=="sparseMatrix"))==0){
-    stop("NN is not a sparse matrix. Please refer the Matrix R package to convert it.")
-  }
-  if ( ncol(NNmatrix)!=nrow(ymat) ){
-    stop("The number of columns of NN and the number of rows of ymat needs to be the same (# voxels).")
+# ymat        : a V times N matrix for the first modality (V: # of vertices, N: # of images)
+# ymat        : a V times N matrix for the second modality (V: # of vertices, N: # of images)
+# mod         : a N times p covariate matrix (p: # of covariates)
+#             : It can be generated easily by using the model.matrix() function.
+# distmat     : a V times V distance matrix
+# sacf        : spatial autocorrelation function
+#             : The exponential function is assumed as a default.
+#             : Other choices include "gau" (Gaussian) 
+#             : and "mix" (mixture of exponential and Gaussian)
+# max.radius  : The maximum radius for cluster enhancement. 20 is assumed as a default.
+# nperm       : number of permutations to be used. At least 5000 permutation is recommended.
+# alpha       : A desired FWER. alpha=0.05 is assumed as a default.
+# alternative : A direction of the alternative hypothesis. A two-sided testing is assumed as a default.
+# seed        : A random seed to be used for permutation.
+#             : It is important to use the same seed for integrating results from both hemispheres.
+# parallel    : Whether parallel computing is to be used.
+# cores       : The number of cores when parallel computing is executed.
+
+CleanR=function(xmat, 
+                ymat,
+                mod = NULL,
+                distmat = NULL, 
+                sacf = "exp",
+                max.radius = 20,
+                nperm = 5000, 
+                alpha = 0.05,
+                alternative = c("two.sided", "less", "greater"),
+                seed = NULL, 
+                npartition = NULL, 
+                parallel = F, 
+                ncores = 1){
+  
+  if ( nrow(NNmatrix) != nrow(ymat) ){
+    stop("The number of rows of NN and the number of rows of ymat needs to be the same (# voxels).")
   }
   if ( alpha<0 | alpha>1){
     stop("alpha should range between 0 and 1.")
   }
-  if (length(alternative)>1){
-    alternative="two.sided"
+  if (length(alternative) > 1){
+    alternative = "two.sided"
     cat("Conducting the two-sided test as alternative has not been specified...\n")
   }
-  if (is.null(seed)){ seed=sample(1e6,1) }
+  if (is.null(seed)){ 
+    seed=sample(1e6,1) 
+  }
+
+  ymat.leverage = spLeverage(ymat, distmat, mod)
+  xmat.leverage = spLeverage(xmat, distmat, mod)
+  NNmatrix = buildNNmatrixDist(distmat, max.radius = max.radius)
   
   # xmat=t(apply(xmat,1,scale))/ncol(xmat)
   # ymat=t(apply(ymat,1,scale))/ncol(ymat)
@@ -23,31 +55,31 @@ CleanR=function(xmat, ymat, NNmatrix, nperm=5000, alpha=0.05,
   for (i in 1:npartition){
     start=len*(i-1)+1
     end=min(len*i,nrow(NNmatrix))
-    NNList[[i]]=NNmatrix[start:end,]
+    NNList[[i]] = NNmatrix[start:end,]
   }
   
-  CleanerPerm=CleanerPermC(xmat, ymat, nperm, seed)
+  CleanerPerm = CleanerPermC(xmat.leverage, ymat.leverage, nperm, seed)
   if (isTRUE(parallel)){
-    cl=makeCluster(ncores)
+    cl = makeCluster(ncores)
     registerDoParallel(cl)
-    result=foreach(i=1:npartition, .packages=("Clean"))%dopar%{
-      fit=CleanerExpandPermC(CleanerPerm$U, CleanerPerm$permU, NNList[[i]])
-      fit$alternative=alternative
-      fit$seed=seed
+    result = foreach(i = 1:npartition, .packages=("CLEAN"))%dopar%{
+      fit = CleanerExpandPermC(CleanerPerm$U, CleanerPerm$permU, NNList[[i]])
+      fit$alternative = alternative
+      fit$seed = seed
       fit
     }
     stopCluster(cl)
   } else{
     result=list()
     for (i in 1:npartition){
-      result[[i]]=CleanerExpandPermC(CleanerPerm$U, CleanerPerm$permU, NNList[[i]])
-      result[[i]]$alternative=alternative
-      result[[i]]$seed=seed
+      result[[i]] = CleanerExpandPermC(CleanerPerm$U, CleanerPerm$permU, NNList[[i]])
+      result[[i]]$alternative = alternative
+      result[[i]]$seed = seed
     }  
     # out=combine(result, alpha=alpha)
     # out$nlocations=ncol(NNmatrix)
-    result=combine(result, alpha=alpha)
-    result$nlocations=ncol(NNmatrix)
+    result = combine(result, alpha = alpha)
+    result$nlocations = ncol(NNmatrix)
   }
   
   
